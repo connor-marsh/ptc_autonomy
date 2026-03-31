12beead4d3d9b35aeaf9803968b6380a0e175b9c
Connor Marsh <connorbmarsh1000@gmail.com>
2026-03-30 20:04:10 -0400

    changed delays on laser odom launch

--- launch/laser_odom.launch.py
+++ launch/laser_odom.launch.py
@@ -8,7 +8,22 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    # 1. Lidar
+    # 1. Static TF
+    tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '-1.5708', '0', '0', 'base_link', 'laser']
+    )
+
+    # 2. RF2O
+    rf2o_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('rf2o_laser_odometry'), 'launch', 'rf2o_laser_odometry.launch.py')
+        )
+    )
+
+    # 3. Lidar
+    # Lidar goes last and is delayed to prevent RF2O from getting the transform messed up
     lidar_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('sllidar_ros2'), 'launch', 'sllidar_a1_launch.py')
@@ -16,23 +31,9 @@ def generate_launch_description():
         launch_arguments={'serial_port': '/dev/ttyLidar'}.items()
     )
 
-    # 2. Static TF
-    tf_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=['0', '0', '0', '-1.5708', '0', '0', 'base_link', 'laser']
-    )
-
-    # 3. RF2O (Delayed)
-    rf2o_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('rf2o_laser_odometry'), 'launch', 'rf2o_laser_odometry.launch.py')
-        )
-    )
-
 
     return LaunchDescription([
-        lidar_node,
         tf_node,
-        TimerAction(period=4.0, actions=[rf2o_launch])
+        rf2o_launch,
+        TimerAction(period=4.0, actions=[lidar_node])
     ])
